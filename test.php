fbe6c57ff78202d6f5c1722d9cfd9a87cf4e7a32
Lennon T Acosta <lennon@ltacosta.com>
2014-01-21 15:04:15 -0500

    Reorganize files. Implement basic php templating. Start adding javascript image gallery.

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php echo file_get_contents("header.html"); ?>
+
+<h2>Test Page</h2>
+
+This is a test page.
+
+<?php echo file_get_contents("footer.html"); ?>
+
--- testimonials.php
+++ testimonials.php
@@ -1,42 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Rent-A-Pup</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
-    <link href="style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-
-<div id="container">
-
-	<div id="header">
-		<div id="title" class="content">
-        	<h1>Rent-A-Pup!</h1>
-        </div>    
-	</div>
-    
-    <div id="nav">
-    		<div id="menu" class="content">
-            	<div class="cell">
-    				<div class="navitem"><a href="index.php">Home</a></div>
-    			</div>
-                <div class="cell">
-    				<div class="navitem"><a href="meet.php">Meet Our Pups</a></div>
-    			</div>
-                <div class="cell">
-    				<div class="navitem"><a href="rent.php">Rent!</a></div>
-    			</div>
-                <div class="cell">
-    				<div class="navitem"><a href="testimonials.php">Testimonials</a></div>
-    			</div>
-                <div class="cell">
-    				<div class="navitem"><a href="contact.php">Contact Us</a></div>
-    			</div>
-            </div>
-		</div>
-
-	<div id="content" class="content">
+<?php echo file_get_contents("header.html"); ?>
 
         <h2>Testimonials!</h2>
             <center> <i>
@@ -47,13 +9,4 @@
             "This was the greatest idea ever. I'm not allowed to have a puppy in my apartment, so it's nice to be able to play with one a few hours here and there."
             </i></center>
     
-    </div>
-
-	<div id="footer" class="content">
-		
-	</div>
-
-</div>
- 
-</body>
-</html>
+<?php echo file_get_contents("footer.html"); ?>
